5692a3b00091c8422326e152e9835f6332130fe5
Gmanlove <emmagman3@gmail.com>
2023-03-15 13:49:59 +0100

    Add popup comments

--- src/modules/display-pokemon.js
+++ src/modules/display-pokemon.js
@@ -1,4 +1,5 @@
 import icon from '../images/heart-regular.svg';
+import commentsPopup from './commentsPopup.js';
 
 const pokdexContainer = document.getElementById('pokdex');
 const displayPokemon = (pokemon) => {
@@ -6,12 +7,25 @@ const displayPokemon = (pokemon) => {
       <img class="card-image" src="${pokemon.image}"/>
       <div class="card-body">
         <h2 class="card-title">${pokemon.name} ${pokemon.id}</h2>
-        <a class="heart" href="#"><img class="heart-img" src="${icon}"></a>
+        <a class="heart" href="#"><img id=${pokemon.id} class="heart-img" src="${icon}"></a>
       </div>
       <div class="like-cont"><span class="likes">n likes</span></div>
-      <button class="comment-btn">Comments</button>
+      <button id="${pokemon.id}" class="comment-btn">Comments</button>
     </li>`).join(', ');
   pokdexContainer.innerHTML = pokemonHTMLString;
+
+  const commentBtn = document.querySelectorAll('.comment-btn');
+  commentBtn.forEach((btn) => {
+    btn.addEventListener('click', async (e) => {
+      const { id } = e.target;
+      const popupCont = document.querySelector('.popup-cont');
+      popupCont.classList.add('active');
+      const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+      const response = await fetch(url);
+      const pokemon = await response.json();
+      commentsPopup(pokemon);
+    });
+  });
 };
 
 export default displayPokemon;
